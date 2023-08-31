5ed13d1d6e3eff4fad1b90e779c0bed6cb5ad6d2
vimala-siravi <vimala1995@gmail.com>
2023-08-31 14:07:12 +0530

    updated rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
 import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default [
   {
