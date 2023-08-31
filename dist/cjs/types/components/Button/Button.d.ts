6c2a4874e50991ccf3a9b15f7850ccc99b21dedb
vimala-siravi <vimala1995@gmail.com>
2023-08-31 14:07:21 +0530

    added new build

--- dist/cjs/types/components/Button/Button.d.ts
+++ dist/cjs/types/components/Button/Button.d.ts
@@ -1,6 +1,3 @@
 import React from "react";
-export interface ButtonProps {
-    label: string;
-}
-declare const Button: (props: ButtonProps) => React.JSX.Element;
+declare const Button: (props: any) => React.JSX.Element;
 export default Button;
