d8f22822d879996abc43d1f1d2b2b54b64ed8364
vimala-siravi <vimala1995@gmail.com>
2023-08-31 13:05:14 +0530

    added jsdom ,babel, terser and moved a few dependencies to peerDependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
+import terser from "@rollup/plugin-terser";
 
 export default [
   {
@@ -20,9 +21,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
     ],
   },
   {
